09534e5a07be4269111acaf5a70fafd36d6a1cd9
James Ingold <jamesingold@Jamess-MacBook-Pro.local>
2016-12-14 15:18:30 -0600

    cleaning up index.js

--- index.js
+++ index.js
@@ -9,9 +9,4 @@
     return dasq.signals().then(function(result) {
         console.log(result);
     });
-    // return dasq.devices().then(function(devices) {
-    //     return dasq.createSignal('Test Signal', devices[1].pid, devices[1].zones[0].id, '#00F').then(function(result) {
-    //         return console.log(result);
-    //     });
-    // });
 })();
